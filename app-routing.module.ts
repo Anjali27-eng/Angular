214a30119e441e8daa723f6d282ee213472e400d
AR <rajoleanjali04@gmail.com>
2022-05-30 20:58:12 +0530

    Create app-routing.module.ts

---
+++ app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './my-component/login/login.component';
+
+import { RegistrationComponent } from './my-
+component/registration/registration.component';
+
+const routes: Routes = [
+{component:RegistrationComponent, path:'registration'},
+{component:LoginComponent , path:'login'}
+];
+@NgModule({
+imports: [RouterModule.forRoot(routes)],
+exports: [RouterModule]
+})
